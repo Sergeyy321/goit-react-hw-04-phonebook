daef50570a5fbd67d2633e72db4ebba45faf11c0
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-09-08 22:38:21 +0200

    commit one

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,84 +1,86 @@
 import { Filter } from 'components/Filter/Filter';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Contacts } from 'components/Contacts/Contacts';
 import {Phonebook } from 'components/Phonebook/Phonebook';
 import { nanoid } from 'nanoid';
-import { ContactStyled} from './App.styled';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+import { ContactStyled } from './App.styled';
+export const App = () => {
+ 
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ])
+  const [filter, setFilter] = useState('')
+
+
+  useEffect(() => {
+  
+      const savedContacts = localStorage.getItem('contacts');
+      if (savedContacts !== null) {
+        const contacts = JSON.parse(savedContacts);
+      setContacts({contacts})
+      }
+  } 
+,[])
+ 
+  useEffect(() => {
+  
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+      
+  }
+,[contacts])
+
+  const onDelete = contactId => {
+    setContacts(prev => prev.filter(contact => contact.id !== contactId))
+  
   };
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      const contacts = JSON.parse(savedContacts);
-    this.setState({contacts})
-    }
-}
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-}
-
-  onDelete = contactId => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const onfilterChange = e => {
+    setFilter(e.currentTarget.value)
+    
   };
 
-  onfilterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  onAddContact = ({ name, number }) => {
+  const onAddContact = ({ name, number }) => {
     if (
-      this.state.contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase()
+      contacts.find(
+        contacts => contacts.name.toLowerCase() === name.toLowerCase()
       )
     ) {
       alert(`${name} is already in contacts`);
     } else {
-      this.setState(prev => ({
-        contacts: [
-          ...prev.contacts,
-          { id: nanoid(), name: name, number: number },
-        ],
-      }));
-    }
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-   
-
-    const lowercaseName = filter.toLowerCase();
-    const contactsEl =
-      this.state.filter !== ''
-        ? contacts.filter(contacts =>
-            contacts.name.toLowerCase().includes(lowercaseName)
-          )
-        : [];
-    return (
-      <ContactStyled>
-        <Phonebook onSubmit={this.onAddContact} />
-        <div>
-            <h2>Contacts</h2>
-          <Filter filter={filter} onfilterChange={this.onfilterChange} />
-          <Contacts
-            contacts={contactsEl}
-            onDelete={this.onDelete}
-            filteredName={filter}
-          />
-        </div>
-      </ContactStyled>
-    );
+      setContacts(prev => [
+        ...prev,
+        { id: nanoid(), name: name, number: number },
+      ])
+    
+    };
   }
+  
+
+ 
+  const lowercaseName = filter.toLowerCase();
+  const contactsEl =
+    filter !== ''
+      ? contacts.filter(contacts =>
+        contacts.name.toLowerCase().includes(lowercaseName)
+      )
+      : [];
+
+  return (
+    <ContactStyled>
+      <Phonebook onSubmit={onAddContact} />
+      <div>
+        <h2>Contacts</h2>
+        <Filter filter={filter} onfilterChange={onfilterChange} />
+        <Contacts
+          contacts={contactsEl}
+          onDelete={onDelete}
+          filteredName={filter}
+        />
+      </div>
+    </ContactStyled>
+  );
 }
+
